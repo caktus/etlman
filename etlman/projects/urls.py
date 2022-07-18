712d3f3cbf51e145ebf42abdf855936a7e833254
Victor Panisa <victor.panisa@gmail.com>
2022-07-18 11:48:17 -0300

    precommit is angry sometimes

--- etlman/projects/urls.py
+++ etlman/projects/urls.py
@@ -5,6 +5,4 @@ from etlman.projects.views import edit_script_view
 # from etlman.projects.views import user_detail_view, user_redirect_view, user_update_view
 
 app_name = "projects"
-urlpatterns = [
-    path("monaco", view=edit_script_view, name="monaco")
-]
+urlpatterns = [path("monaco", view=edit_script_view, name="monaco")]
