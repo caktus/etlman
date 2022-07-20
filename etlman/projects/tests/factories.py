f3fd31ef74d9f2b3996b4357a7b11446115ba792
Victor Panisa <victor.panisa@gmail.com>
2022-07-20 16:05:36 -0300

    Reordering the factories to avoid hard-coding the full path

--- etlman/projects/tests/factories.py
+++ etlman/projects/tests/factories.py
@@ -4,34 +4,6 @@ from etlman.projects import models
 from etlman.users.tests.factories import UserFactory
 
 
-class PipelineFactory(factory.django.DjangoModelFactory):
-    project = factory.SubFactory("etlman.projects.tests.factories.ProjectFactory")
-    name = factory.Faker("name")
-
-    class Meta:
-        model = models.Pipeline
-
-
-class StepFactory(factory.django.DjangoModelFactory):
-    pipeline = factory.SubFactory("etlman.projects.tests.factories.PipelineFactory")
-    name = factory.Faker("name")  # file name
-    script = factory.Faker("pystr")  # payload
-    step_order = factory.Sequence(lambda n: n)
-
-    class Meta:
-        model = models.Step
-
-
-class DataInterfaceFactory(factory.django.DjangoModelFactory):
-    project = factory.SubFactory("etlman.projects.tests.factories.ProjectFactory")
-    name = factory.Faker("company")
-    interface_type = factory.Faker("company_suffix")
-    connection_string = factory.Faker("sentence")
-
-    class Meta:
-        model = models.DataInterface
-
-
 class ProjectFactory(factory.django.DjangoModelFactory):
     name = factory.Faker("name")
     description = factory.Faker("sentence")
@@ -47,3 +19,31 @@ class CollaboratorFactory(factory.django.DjangoModelFactory):
 
     class Meta:
         model = models.Collaborator
+
+
+class PipelineFactory(factory.django.DjangoModelFactory):
+    project = factory.SubFactory(ProjectFactory)
+    name = factory.Faker("name")
+
+    class Meta:
+        model = models.Pipeline
+
+
+class DataInterfaceFactory(factory.django.DjangoModelFactory):
+    project = factory.SubFactory(ProjectFactory)
+    name = factory.Faker("company")
+    interface_type = factory.Faker("company_suffix")
+    connection_string = factory.Faker("sentence")
+
+    class Meta:
+        model = models.DataInterface
+
+
+class StepFactory(factory.django.DjangoModelFactory):
+    pipeline = factory.SubFactory(PipelineFactory)
+    name = factory.Faker("name")  # file name
+    script = factory.Faker("pystr")  # payload
+    step_order = factory.Sequence(lambda n: n)
+
+    class Meta:
+        model = models.Step
