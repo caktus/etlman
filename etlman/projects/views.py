e0d7dc118a1251f632f95d3b688ba2fa27299b83
Victor Panisa <victor.panisa@gmail.com>
2022-07-28 21:04:07 -0300

    Addes message enum

--- etlman/projects/views.py
+++ etlman/projects/views.py
@@ -1,3 +1,5 @@
+from email.message import Message
+from enum import Enum
 from django.contrib import messages
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
@@ -7,20 +9,28 @@ from etlman.projects.forms import StepForm
 from etlman.projects.models import Step
 
 
+class MessagesEnum:
+    STEP_UPDATED = "Step updated!"
+    STEP_CREATED = "New step script added succesfully!"
+
 def step_form_upsert_view(request, pk=None):
     loaded_obj = Step.objects.get(id=pk) if pk else None
     if request.method == "POST":
         form = StepForm(request.POST, instance=loaded_obj)
         if form.is_valid():
+            
             saved_obj = form.save()
             messages.add_message(
                 request,
                 messages.SUCCESS,
-                "Step updated!" if loaded_obj else "New step script added succesfully!",
+                MessagesEnum.STEP_UPDATED if loaded_obj else MessagesEnum.STEP_CREATED,
             )
             return HttpResponseRedirect(
                 reverse("projects:step_form_upsert", args=[saved_obj.id])
             )
+        else:
+            ...
+            #breakpoint()
     else:  # GET
         form = StepForm(instance=loaded_obj)
     context = {"form": form}
