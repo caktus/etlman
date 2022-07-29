d8598f1770aca43c2f58f2a41b6c78b42f351380
Victor <5880370+wfzyx@users.noreply.github.com>
2022-07-29 15:10:01 -0300

    Updates etlman/projects/urls.py
    Co-authored-by: Tobias McNulty <tobias@caktusgroup.com>

--- etlman/projects/urls.py
+++ etlman/projects/urls.py
@@ -5,5 +5,5 @@ from etlman.projects.views import step_form_upsert_view
 app_name = "projects"
 urlpatterns = [
     path("step_form/", view=step_form_upsert_view, name="step_form_upsert"),
-    path("step_form/<str:pk>", view=step_form_upsert_view, name="step_form_upsert"),
+    path("step_form/<str:pk>/", view=step_form_upsert_view, name="step_form_upsert"),
 ]
