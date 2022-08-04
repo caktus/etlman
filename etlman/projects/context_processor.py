8dc1b331211d348ec81b6e9f0d792b45dbe4501a
ronardcaktus <rluna@caktusgroup.com>
2022-08-04 16:14:23 -0400

    Create a context processor, add projects to navbar, and relevant links

---
+++ etlman/projects/context_processor.py
@@ -0,0 +1,14 @@
+from etlman.projects.models import Project
+
+
+def currrent_user_projects(request):
+    """
+    Filters all projects and returns those that belong
+    to the current user.
+    """
+    username = request.user
+    return {
+        "current_user_projects": Project.objects.all().filter(
+            collaborators=username.id
+        ),
+    }
