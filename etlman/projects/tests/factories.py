c434e6fed542c8f7ee33198091eaf366a7583d8d
Victor Panisa <victor.panisa@gmail.com>
2022-08-05 13:16:08 -0300

    Fixes factories

--- etlman/projects/tests/factories.py
+++ etlman/projects/tests/factories.py
@@ -21,22 +21,22 @@ class CollaboratorFactory(factory.django.DjangoModelFactory):
         model = models.Collaborator
 
 
-class PipelineFactory(factory.django.DjangoModelFactory):
-    project = factory.SubFactory(ProjectFactory)
-    name = factory.Faker("name")
-
-    class Meta:
-        model = models.Pipeline
-
-
 class DataInterfaceFactory(factory.django.DjangoModelFactory):
-    project = factory.SubFactory(ProjectFactory)
     name = factory.Faker("company")
     interface_type = factory.Faker("company_suffix")
     connection_string = factory.Faker("sentence")
 
     class Meta:
         model = models.DataInterface
+
+
+class PipelineFactory(factory.django.DjangoModelFactory):
+    project = factory.SubFactory(ProjectFactory)
+    name = factory.Faker("name")
+    input = factory.SubFactory(DataInterfaceFactory)
+
+    class Meta:
+        model = models.Pipeline
 
 
 class StepFactory(factory.django.DjangoModelFactory):
