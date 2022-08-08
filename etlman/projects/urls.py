fe07c6998ddd406ede5f502eaa1bca1e9add0971
Victor <5880370+wfzyx@users.noreply.github.com>
2022-08-08 12:00:59 -0300

    Update etlman/projects/urls.py
    Co-authored-by: Tobias McNulty <tobias@caktusgroup.com>

--- etlman/projects/urls.py
+++ etlman/projects/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path("step_form/", view=step_form_upsert_view, name="step_form_upsert"),
     path("step_form/<str:pk>/", view=step_form_upsert_view, name="step_form_upsert"),
     path(
-        "add_project_wizard/", view=new_project_wizard_view, name="add_project_wizard"
+        "new-project/", view=new_project_view, name="new_project"
     ),
 ]
