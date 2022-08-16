74f71279cb64c6ff4f1a025f4bbe701e6190bf89
Victor Panisa <victor.panisa@gmail.com>
2022-08-15 23:50:52 -0300

    Removes mypy annotation errors

--- etlman/projects/authorizers.py
+++ etlman/projects/authorizers.py
@@ -6,7 +6,7 @@ def user_is_authenticated(request: HttpRequest, **view_kwargs: dict) -> bool:
 
 
 def user_is_project_collaborator(request: HttpRequest, **view_kwargs: dict) -> bool:
-    project_id = view_kwargs["project_id"]
+    project_id = str(view_kwargs["project_id"])
     return (
         request.user.is_authenticated
         and request.user.projects.filter(pk=project_id).exists()
