08f5aa0fb13e65f540d8ce697f54b0b95680c1a4
ronardcaktus <rluna@caktusgroup.com>
2022-08-25 10:38:10 -0400

    Improve authorizers function

--- etlman/projects/authorizers.py
+++ etlman/projects/authorizers.py
@@ -1,12 +1,16 @@
+from typing import Union
+
 from django.http import HttpRequest
 
 
-def user_is_authenticated(request: HttpRequest, **view_kwargs: dict) -> bool:
+def user_is_authenticated(request: HttpRequest, **view_kwargs: Union[str, int]) -> bool:
     return request.user.is_authenticated
 
 
-def user_is_project_collaborator(request: HttpRequest, **view_kwargs: dict) -> bool:
-    project_id = str(view_kwargs["project_id"])
+def user_is_project_collaborator(
+    request: HttpRequest, **view_kwargs: Union[str, int]
+) -> bool:
+    project_id = view_kwargs["project_id"]
     return (
         request.user.is_authenticated
         and request.user.projects.filter(pk=project_id).exists()
