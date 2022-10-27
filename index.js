055dfad85ae8474c873fd085848bc559dd029a9f
janelle-bouchard <jbouchard@caktusgroup.com>
2022-10-27 14:04:26 -0400

    Updated index js and monaco widget

--- index.js
+++ index.js
@@ -3,12 +3,4 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import * as monaco from 'monaco-editor';
 
-let monacoEditor = monaco.editor.create()
-export function getMonacoValue() {
-    return monacoEditor.getModel().getValue()
-}
-
-export const selectedLanguage = lang => {
-   monaco.editor.setModelLanguage(monacoEditor.getModel(), lang);
-}
-selectedLanguage(document.getElementById("id_language").value);
+console.log("index")
