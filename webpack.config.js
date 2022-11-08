f94e3b39f0d22eecf38da2bacc6ee94978f5f3c1
janelle-bouchard <jbouchard@caktusgroup.com>
2022-11-08 10:39:03 -0500

    Deleted gulp and moved everything to webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,47 @@
 const path = require('path');
 const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
-
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 
 module.exports = {
 	entry: './index.js',
 	mode: 'development',
 	output: {
-		path: path.resolve(__dirname, 'etlman/static/js'),
-		filename: 'app.js'
+		path: path.resolve(__dirname, 'etlman/static'),
+		filename: 'js/app.js',
 	},
+	optimization: {
+		minimizer: [
+			new CssMinimizerPlugin(),
+		  ],
+		minimize: true,
+		splitChunks: {
+		  cacheGroups: {
+			styles: {
+			  name: "styles",
+			  type: "css/mini-extract",
+			  chunks: "all",
+			  enforce: true,
+			},
+		  },
+		},
+	  },
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
-				use: ['style-loader', 'css-loader']
-			},
+				use: [MiniCssExtractPlugin.loader, "css-loader"],
+				},
             {
 				test: /\.ttf$/,
 				type: 'asset/resource'
 			}
 		]
 	},
-	plugins: [new MonacoWebpackPlugin()]
+	plugins: [
+		new MonacoWebpackPlugin(),
+		new MiniCssExtractPlugin({
+			filename: "css/project.css",
+		  }),
+	]
 }
